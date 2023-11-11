927cf5f3032125e0cf6221dd6d95f9e44777fd87
CarolusFPV <casper.m@hotmail.nl>
2023-11-11 14:37:23 +0100

    Friend list for normal egg turner now gets randomized for better credit per user stats

--- userscript.js
+++ userscript.js
@@ -85,6 +85,13 @@ class TurnEggsModule extends OviPostModule {
                 break;
             }
         }
+
+        // Randomize the friends array using Fisher-Yates Shuffle
+        for (let i = friends.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [friends[i], friends[j]] = [friends[j], friends[i]];
+        }
+
         return friends;
     }
 
